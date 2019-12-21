7612b7c0adc3690ec62a117266a72bd0b93dc8c0
MiVladie <mivladie@github.com>
2019-12-21 19:26:42 +0200

    Updates

---
+++ precache-manifest.48e1be3ab6353f0987496ce9cf46b25c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b480f38d374d23f920e6338cf9fd0bf",
+    "url": "/beaute/index.html"
+  },
+  {
+    "revision": "d681aab86322906a9bb1",
+    "url": "/beaute/static/css/main.e3246ee1.chunk.css"
+  },
+  {
+    "revision": "5987b5ed23972d6217a1",
+    "url": "/beaute/static/js/2.54b81468.chunk.js"
+  },
+  {
+    "revision": "d681aab86322906a9bb1",
+    "url": "/beaute/static/js/main.bf06259b.chunk.js"
+  },
+  {
+    "revision": "47d39199c797bad7d355",
+    "url": "/beaute/static/js/runtime-main.cd003ad0.js"
+  },
+  {
+    "revision": "8e4623416c38ac597b76c8e8ae5165e9",
+    "url": "/beaute/static/media/banner-01.8e462341.png"
+  },
+  {
+    "revision": "6400576b201c24a3fb37f4ed393d7958",
+    "url": "/beaute/static/media/banner-02.6400576b.png"
+  },
+  {
+    "revision": "fa404fa5b675f5e1f11f94c04126d472",
+    "url": "/beaute/static/media/info1.fa404fa5.jpg"
+  },
+  {
+    "revision": "1b29d3b7093b8629bc5436de154c5a46",
+    "url": "/beaute/static/media/info2.1b29d3b7.jpg"
+  },
+  {
+    "revision": "29df23860216db3cbeeb9d6083782962",
+    "url": "/beaute/static/media/member1.29df2386.jpg"
+  },
+  {
+    "revision": "b2f6d11dd445e04a175f335c71a6e309",
+    "url": "/beaute/static/media/member2.b2f6d11d.jpg"
+  },
+  {
+    "revision": "5448039e896e365a6360e59124cb1097",
+    "url": "/beaute/static/media/member3.5448039e.jpg"
+  },
+  {
+    "revision": "adfb62186ccc1161d2fce82c33109d9f",
+    "url": "/beaute/static/media/member4.adfb6218.jpg"
+  },
+  {
+    "revision": "9c549f1e7306338258d4b64d7bb9ef08",
+    "url": "/beaute/static/media/salon.9c549f1e.jpg"
+  }
+]);
